65fa4f5b99f52a645ddcffad90cf0fd36a3de22a
Pav0l <pavol.karas@gmail.com>
2019-05-08 22:44:49 +0200

    Make req.body leaner

--- popup/index.js
+++ popup/index.js
@@ -1,11 +1,10 @@
-
 // target submit button and add click event listener
 document.getElementById('submit-1').addEventListener('click', () => {
   // select all checked input field
   const checkedField = document.querySelector('input.radio-btn:checked');
 
   // get the URI and value from the checked input Node
-  const result = { value: checkedField.value };
+  const result = { rating: checkedField.value };
   const url = 'http://127.0.0.1:9000/api/rating';
 
   // eslint-disable-next-line no-undef
@@ -13,14 +12,14 @@ document.getElementById('submit-1').addEventListener('click', () => {
     console.log('tabs', tabs);
     const activeTab = tabs.filter(tab => tab.active === true);
 
-    result.tab = activeTab[0];
+    result.url = activeTab[0].url;
 
     fetch(url, {
       method: 'POST',
       body: JSON.stringify(result),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        'Content-Type': 'application/json',
+      },
     })
       .then(res => console.log({ ok: res.ok }))
       .catch(err => console.error(err))
@@ -32,5 +31,3 @@ document.getElementById('submit-1').addEventListener('click', () => {
     event.preventDefault();
   });
 });
-
-
